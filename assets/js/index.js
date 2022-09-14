413a50272f234896db384e7fdf8a242421b80f70
BaByHaRdEn <mangergeorgepraise@gmail.com>
2022-09-14 08:23:33 +0100

    history page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+let history=document.getElementById("history");
+
 function myFunction() {
     var x = document.getElementById("navigation");
     if (x.style.display === "block") {
@@ -5,4 +7,17 @@ function myFunction() {
     } else {
       x.style.display = "block";
     }
+  }
+
+  let goBack=()=> {
+    window.history.back()
+  }
+
+  if(history.innerHTML !== null){
+    history.innerHTML= `
+    <p><i class="fa fa-history"></i></p>
+
+    <h4>You have not appllied for any exeat</h4>
+    <button class="btn btn-danger " onclick="goBack()">Apply for exeat<i class="fa fa-angle-double-right"></i></button>
+    `
   }
